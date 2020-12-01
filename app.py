f86de878d8138b0970f0f1ac056e71dff1d06470
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-12-01 15:54:59 -0300

    Fix Bugs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import db
+from models import db, User
 from dotenv import load_dotenv
 from os import environ
 
