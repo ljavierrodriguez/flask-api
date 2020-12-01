2b2fbc8d64dda399bb3cfcdf99d45396ea54dea7
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-12-01 15:09:17 -0300

    Add Postgresql Database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from models import db
 from dotenv import load_dotenv
 from os import environ
 
@@ -9,7 +10,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@{}/{}'.format(environ.get("DBUSER"), environ.get("DBPASS"), environ.get("DBHOST"), environ.get("DBNAME")))
 manager = Manager(app)
 
 
@@ -21,3 +23,11 @@ def main():
 if __name__ == '__main__':
     manager.run()
 
+'''
+
+Si se establece en True, Flask-SQLAlchemy rastreará las modificaciones de los objetos y emitirá señales. 
+El valor predeterminado es None, que habilita el seguimiento, pero emite una advertencia de que se 
+deshabilitará de manera predeterminada en el futuro. Esto requiere memoria adicional y debe desactivarse 
+si no es necesario.
+
+'''
